77f23ae3f539af3acbd5283ccf5a5265ad8a60fd
Emre-KARASU <yargiiiic@gmail.com>
2022-12-12 20:12:15 +0300

    Duzeltme

--- src/day02_variables/C01_Variables.java
+++ src/day02_variables/C01_Variables.java
@@ -2,7 +2,7 @@ package day02_variables;
 public class C01_Variables {
     public static void main(   String[] args   ) {
         // bir variable olustururken icerisine koyacagim datanin alabilecegi degerlere uygun
-        // bir data turu saecmeliyiz
+        // bir data turu secmeliyiz
         // ornegin bir sehrin nufusundan bahsediyorsak
         // variable'imizin data turu String, boolean, char veya double olamaz
         // geriye kalan tamsayi turlerinden sehrin nufusunu icine alabilecek buyuklukte bir data turu seceiliriz
